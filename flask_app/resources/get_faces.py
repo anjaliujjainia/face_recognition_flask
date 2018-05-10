ee7d6da7d641f64c9eb89567264e67bc26c38c45
Anjali Ujjainia <ujjainia.anjali@tftus.com>
2018-05-10 15:28:15 +0530

    added exceptions to stop avoiding fail code

--- flask_app/resources/get_faces.py
+++ flask_app/resources/get_faces.py
@@ -15,7 +15,9 @@ from skimage import io
 import hashlib
 import face_recognition
 from urllib.request import urlopen
+import urllib#.request import urlopen
 from werkzeug.utils import secure_filename
+import pdb
 
 photo_location = app.config['LOCATION']
 face_location = app.config['FACE_LOCATION']
@@ -38,10 +40,12 @@ def save_face_img(id, img, who='face'):
 
         # changing the color format of Image from BGR to RGB 
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-    # saving the thumbnail of the face at img_path
-    if cv2.imwrite(img_path, img):
-        return img_path
+    try:
+        # saving the thumbnail of the face at img_path
+        if cv2.imwrite(img_path, img):
+            return img_path
+    except:
+        print("Could not save " + who)
 
 # ----------- Generate hash of Photo ---------------------
 def generate_md5(image_path):
@@ -61,95 +65,111 @@ class GetFaces(Resource):
         if len(data) > 0:
             
             faceNames = []
-
+            errorImages = []
+            imagesInDB = []
             for image in data.items():
             # INDENTATION START
                 ruby_image_id = image[0]                        #randrange(0, 100)#
                 imageUrl = image[1]["url"]
                 group_id = image[1]["group_id"]                         #randrange(0, 100)
                 caption = "RANDOM"                              #args['caption']
-                
-                img = getImageFromURL(imageUrl) # for url image
-                # ---------------------- SAVING IMAGE -----------------
-                photoId = str(uuid.uuid4())
-                img_local_path = save_face_img(photoId, img, who='photo')
-                # Hold url of images which were not readable
-                errorImages = []
                 try:
-                    image = face_recognition.load_image_file(img_local_path)
-                    image_hash = generate_md5(img_local_path)
-                    os.remove(img_local_path)
-                except Exception as e:
-                    errorImages.append(imageUrl)
-                
-                # Getting All rows from person table and making lists of face ids and encodings
-                person_query = db.session.query(Model.Person).all()
-                knownFaceEncodings = [_.mean_encoding for _ in person_query]
-                knownFaceIds = [_.id for _ in person_query]
-                
-                # Locations of faces in photo
-                faceLocations = face_recognition.face_locations(image)
-                # Encodings of faces in photo
-                faceEncodings = face_recognition.face_encodings(image, faceLocations)
-
-                # -------- Photo ---------
-                # url of image  generate hash and then delete image from local storage
-                image_path = imageUrl
-                
-
-                # # # # ruby_id - ruby_image_id, image_hash, image_url, captions, group_id
-                imagesInDB = []
-                photoObj = db.session.query(Model.Photo).filter_by(image_hash=image_hash).first()
-                if photoObj:
-                    imagesInDB.append(imageUrl)
+                    img = getImageFromURL(imageUrl) # for url image
+                except urllib.error.URLError as err:
+                    print(err.reason)
+                    pdb.set_trace()
+                    print('File Not Found at URL')
+                    errorImages.append(ruby_image_id)
                 else:
-                    photoObj = Model.Photo(image_path, ruby_image_id, image_hash, caption, group_id)
-                    db.session.add(photoObj)
-                    db.session.commit()
-                    # ------- End Photo ------
-                    
-                for i, faceEncoding in enumerate(faceEncodings):
-                    matchedFacesBool = face_recognition.compare_faces(knownFaceEncodings, faceEncoding, tolerance=0.4)
-                    faceId = str(uuid.uuid4())
-                    # If a match was found in known_face_encodings, just use the first one.
-                    if True in matchedFacesBool:
-                        firstMatchIndex = matchedFacesBool.index(True)
-                        matched_id = knownFaceIds[firstMatchIndex]
-                        personObj = db.session.query(Model.Person).filter_by(id=matched_id).first()
-                        personObj.update_average_face_encoding(faceEncoding)
-                        faceNames.append(personObj.name)
+                    print('YAY! File Found and we are decoding it now') 
+                    # ---------------------- SAVING IMAGE -----------------
+                    photoId = str(uuid.uuid4())
+                    img_local_path = save_face_img(photoId, img, who='photo')
+                    # Hold url of images which were not readable
+                    # errorImages = []
+                    try:
+                        pdb.set_trace()
+                        image = face_recognition.load_image_file(img_local_path)
+                        image_hash = generate_md5(img_local_path)
+                        os.remove(img_local_path)
+                    # except urllib.error.HTTPError as err:
+                    except FileNotFoundError as err:
+                        pdb.set_trace()
+                        print(err)
+                        print("Could not load the image from saved Images")
+                        errorImages.append(ruby_image_id)
+                        continue
                     else:
-                        name = "unknown"
-                        personObj = Model.Person(faceEncoding, name, group_id=group_id)
-                        db.session.add(personObj)
-                        db.session.commit()
-                        fetch_person_id = personObj.id
-                    
-                    # Make a face object and save to database with unknown name
-                    top, right, bottom, left = faceLocations[i]
-                    personFace = image[top:bottom, left:right]
+                        # Getting All rows from person table and making lists of face ids and encodings
+                        print("Going in else")
+                        pdb.set_trace()
+                        person_query = db.session.query(Model.Person).all()
+                        knownFaceEncodings = [_.mean_encoding for _ in person_query]
+                        knownFaceIds = [_.id for _ in person_query]
+                        
+                        # Locations of faces in photo
+                        faceLocations = face_recognition.face_locations(image)
+                        # Encodings of faces in photo
+                        faceEncodings = face_recognition.face_encodings(image, faceLocations)
+
+                        # -------- Photo ---------
+                        # url of image  generate hash and then delete image from local storage
+                        image_path = imageUrl
+                        
+
+                        # ruby_image_id, image_hash, image_url, captions, group_id
+                        photoObj = db.session.query(Model.Photo).filter_by(image_hash=image_hash).first()
+                        if photoObj:
+                            imagesInDB.append(imageUrl)
+                        else:
+                            photoObj = Model.Photo(image_path, ruby_image_id, image_hash, caption, group_id)
+                            db.session.add(photoObj)
+                            db.session.commit()
+                            # ------- End Photo ------
+                            
+                        for i, faceEncoding in enumerate(faceEncodings):
+                            matchedFacesBool = face_recognition.compare_faces(knownFaceEncodings, faceEncoding, tolerance=0.4)
+                            faceId = str(uuid.uuid4())
+                            # If a match was found in known_face_encodings, just use the first one.
+                            if True in matchedFacesBool:
+                                firstMatchIndex = matchedFacesBool.index(True)
+                                matched_id = knownFaceIds[firstMatchIndex]
+                                personObj = db.session.query(Model.Person).filter_by(id=matched_id).first()
+                                personObj.update_average_face_encoding(faceEncoding)
+                                faceNames.append(personObj.name)
+                            else:
+                                name = "unknown"
+                                personObj = Model.Person(faceEncoding, name, group_id=group_id)
+                                db.session.add(personObj)
+                                db.session.commit()
+                                fetch_person_id = personObj.id
+                            
+                            # Make a face object and save to database with unknown name
+                            top, right, bottom, left = faceLocations[i]
+                            personFace = image[top:bottom, left:right]
 
 
-                    filename = faceId + '.jpg'
-                    img_path = os.path.join(face_location, filename)
-                    # pdb.set_trace()
-                    faceObj = Model.Face(faceId, photoObj.id, faceEncoding, personObj.id, img_path, top, bottom, left , right)
-                    db.session.add(faceObj)
-                    db.session.commit()
+                            filename = faceId + '.jpg'
+                            img_path = os.path.join(face_location, filename)
+                            # pdb.set_trace()
+                            faceObj = Model.Face(faceId, photoObj.id, faceEncoding, personObj.id, img_path, top, bottom, left , right)
+                            db.session.add(faceObj)
+                            db.session.commit()
 
-                    # if the person is new, set new face as its default
-                    if not True in matchedFacesBool: 
-                        personObj = db.session.query(Model.Person).filter_by(id=fetch_person_id).first()
-                        personObj.default_face = faceObj.id
-                        db.session.commit()
-                    
-                    save_face_img(faceId, personFace, "face")
-                # INDENTATION END
-            # CHECK INDENTATION AND DATA
+                            # if the person is new, set new face as its default
+                            if not True in matchedFacesBool: 
+                                personObj = db.session.query(Model.Person).filter_by(id=fetch_person_id).first()
+                                personObj.default_face = faceObj.id
+                                db.session.commit()
+                            
+                            save_face_img(faceId, personFace, "face")
+                    # INDENTATION END
+                # CHECK INDENTATION AND DATA
 
             if len(faceNames) == 0:
-                return jsonify({"status": 200, "message": "No Known Faces Found.", "Error Images": errorImages, "Same Images": imagesInDB})
+                pdb.set_trace()
+                return jsonify({"status": 200, "message": "No Known Faces Found.", "Error Images": errorImages, "Saved Images": imagesInDB})
 
-            return jsonify({"status": 200, "message": "Faces Found.", "known_faces": str(faceNames), "Error Images": errorImages, "Same Images": imagesInDB})
+            return jsonify({"status": 200, "message": "Faces Found.", "known_faces": str(faceNames), "Error Images": errorImages, "Saved Images": imagesInDB})
         else:
             return jsonify({"status": 406, "message": "Please Provide Data."})
