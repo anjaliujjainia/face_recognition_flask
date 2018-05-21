76b29938f47fd74224a543fae9c3203b21698f43
Anjali Ujjainia <ujjainia.anjali@tftus.com>
2018-05-21 17:32:32 +0530

    minor

--- run_cron.py
+++ run_cron.py
@@ -3,12 +3,12 @@ from django.core.management.base import BaseCommand, CommandError
 from crontab import CronTab
 import os
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cair_project.settings")
+
 #init cron
 cron = CronTab(user=True)
 
-python_path = '~/DjangoProject/venv/bin/python'
-folder = '~/DjangoProject/face_recognition_flask/'
+python_path = '~/FlaskProject/venv/bin/python'
+folder = '~/FlaskProject/face_recognition_flask/'
 file_name1 = folder + 'lazy_delete_people.py'
 file_name2 = 'update_similar_people.py'
 
