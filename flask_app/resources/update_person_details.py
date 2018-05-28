ed8b892fa1d5db49dd1534372efa909d62e71c88
Anjali Ujjainia <ujjainia.anjali@tftus.com>
2018-05-28 17:22:07 +0530

    Updated person if kid_id already exist

--- flask_app/resources/update_person_details.py
+++ flask_app/resources/update_person_details.py
@@ -8,22 +8,31 @@ from flask_app.app import db
 class UpdatePersonDetails(Resource):
     def post(self):
         data = dict(request.get_json(force=True))
+        msg = ''
         if len(data) > 0:
             person_id = data["person_id"]
             person_name = data["person_name"]
             kid_id = data["kid_id"]
-
+            # check if any person with the same kid_id exist
+            # original person
+            o_person = db.session.query(Model.Person).filter_by(kid_id=kid_id).first()
+            o_person_id = o_person.id
             person = db.session.query(Model.Person).filter_by(id=person_id).first()
-            person.name = person_name
-            person.kid_id = kid_id
+            
+            if o_person and o_person_id != person_id:
+                msg =  person_name + " with the kid_id :: " + str(kid_id) +" already exist. Updating :: " + o_person_id
+                o_person.name = person_name
+                # get all the faces of the person_id
+                faces = db.session.query(Model.Face).filter_by(person=person_id).all()
+                for face in faces:
+                    face.person = o_person_id
+                person.lazy_delete = True
+            else:
+                msg = str(person_id) + " is updated with person name as " + person_name + " and kid id as " + str(kid_id)
+                person.name = person_name
+                person.kid_id = kid_id
+            print(msg)
             db.session.commit()
-
-            return jsonify({"status": 200, "message": str(person_id) + " is updated with person name as " + person_name + " and kid id as " + str(kid_id)})
+            return jsonify({"status": 200, "message": msg})
         else:
             return jsonify({"status": 406, "message": "Method Not Allowed with NULL data."})
-
-
-        # parse = reqparse.RequestParser()
-        # parse.add_argument('person_id', type = int)
-        # parse.add_argument('person_name', type = str)
-        # parse.add_argument('kid_id', type = int)
